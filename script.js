8d3fde9494c7ec5f26d0b021dc09e32d09adb7c1
Manoj <114227142+Manoj1408@users.noreply.github.com>
2023-03-31 16:38:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // your code here
+const button = document.getElementById('button');
+  const url = document.getElementById('url');
+  
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    const name = document.getElementById("name").value;
+    const year = document.getElementById("year").value;
+    const queryString = `?name=${name}&year=${year}`;
+
+    url.textContent = url.textContent + queryString;
+  });
